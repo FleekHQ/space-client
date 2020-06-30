62e454eaa9f82181360f4f6a12c37ff9b6c0ab6f
Guillermo Puente <guillermo.ps09@gmail.com>
2020-06-30 17:26:50 -0400

    Added txlSubscribe, openFile, createBucket + docs

--- src/client.ts
+++ src/client.ts
@@ -1,8 +1,14 @@
 import grpcWeb from 'grpc-web';
+import { Empty } from 'google-protobuf/google/protobuf/empty_pb';
 import { SpaceApiClient } from './definitions/SpaceServiceClientPb';
 import {
+  TextileEventResponse,
   ListDirectoriesRequest,
   ListDirectoriesResponse,
+  OpenFileRequest,
+  OpenFileResponse,
+  CreateBucketRequest,
+  CreateBucketResponse,
 } from './definitions/space_pb';
 
 interface SpaceClientOpts {
@@ -32,6 +38,60 @@ class SpaceClient {
         request,
         metadata,
         (err: grpcWeb.Error, res: ListDirectoriesResponse) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+
+          resolve(res);
+        },
+      );
+    });
+  }
+
+  txlSubscribe(
+    metadata: grpcWeb.Metadata = {},
+  ): grpcWeb.ClientReadableStream<TextileEventResponse> {
+    const request = new Empty();
+
+    return this.instance.txlSubscribe(request, metadata);
+  }
+
+  openFile(
+    path: string,
+    metadata: grpcWeb.Metadata = {},
+  ): Promise<OpenFileResponse> {
+    return new Promise((resolve, reject) => {
+      const request = new OpenFileRequest();
+      request.setPath(path);
+
+      this.instance.openFile(
+        request,
+        metadata,
+        (err: grpcWeb.Error, res: OpenFileResponse) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+
+          resolve(res);
+        },
+      );
+    });
+  }
+
+  createBucket(
+    slug: string,
+    metadata: grpcWeb.Metadata = {},
+  ): Promise<CreateBucketResponse> {
+    return new Promise((resolve, reject) => {
+      const request = new CreateBucketRequest();
+      request.setSlug(slug);
+
+      this.instance.createBucket(
+        request,
+        metadata,
+        (err: grpcWeb.Error, res: CreateBucketResponse) => {
           if (err) {
             reject(err);
             return;
